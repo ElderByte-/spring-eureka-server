b6299dbdcb60deaeadeb52330cd35a349c7bf59d
IsNull <p.buettiker@elderbyte.com>
2016-05-29 13:33:29 +0200

    added discovery client

--- src/main/java/com/elderbyte/eureka/EurekaApplicationServer.java
+++ src/main/java/com/elderbyte/eureka/EurekaApplicationServer.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
@@ -16,6 +17,7 @@ import java.net.UnknownHostException;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaApplicationServer {
 
     private static final Logger log = LoggerFactory.getLogger(EurekaApplicationServer.class);
